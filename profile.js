ce7f4e86e7e23cd5de0c693892a9246987a5c73c
vignesh v <vicky20062406@gmail.com>
2026-01-21 15:29:12 +0530

    initial

---
+++ profile.js
@@ -0,0 +1,16 @@
+function Leetcode() {
+    // Replace with your LinkedIn profile URL
+    window.location.href = "https://leetcode.com/u/vignesh242006/";
+  }
+function Github() {
+    // Replace with your GitHub profile URL
+    window.location.href = "https://github.com/vicky200624";
+    }
+function Linkedin() {
+    // Replace with your LinkedIn profile URL
+    window.location.href = "https://www.linkedin.com/in/vignesh-v-107587290?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+  }
+function Whatsapp() {
+    // Replace with your WhatsApp number
+    window.location.href = "https://wa.me/+916369787824?text=Hello%20Vignesh!%20I%20found%20your%20profile%20and%20would%20love%20to%20connect.";
+  }
